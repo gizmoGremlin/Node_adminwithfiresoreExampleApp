3c61306e83b1128a3e85abc66b04dc504df6e493
Nick Gill <nick.gilliosipescu@gmail.com>
2019-05-08 17:09:28 -0300

    Update firestore.js

--- firestore.js
+++ firestore.js
@@ -1,12 +1,6 @@
 const firebase = require('firebase')
 var config = { /* COPY THE ACTUAL CONFIG FROM FIREBASE CONSOLE */
-  apiKey: "AIzaSyB9ZUoj1tKMDBAG44cC9xwVAyTAOPPan98",
-  authDomain: "examplestore-1a436.firebaseapp.com",
-  databaseURL: "https://examplestore-1a436.firebaseio.com",
-  projectId: "examplestore-1a436",
-  storageBucket: "examplestore-1a436.appspot.com",
-  messagingSenderId: "342150795538",
-  appId: "1:342150795538:web:d3609cbe8aa98647"
+ 
 };
 var fire = firebase.initializeApp(config);
 module.exports = fire;
