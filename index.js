a21bcdb414de0d881f0fda2132c03235795b1539
gizmoGremlin <nick.gilliosipescu@gmail.com>
2019-05-10 13:34:07 -0300

    changes to ui

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
 const express = require('express');
 const app =  express();
 const exphbs =  require('express-handlebars');
+const path = require('path');
 
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
+
+app.use(express.static(path.join(__dirname,'public')));
+
+
 app.use('/api/category',require('./routes/api/category'));
 
 app.engine('.hbs', exphbs({extname:'.hbs',defaultLayout: 'main'}));
 app.set('view engine','.hbs');
 
 app.get('/', (req,res)=>{
-        res.render('home',{title:'Post A Category'})
+        res.render('home',{title:'Post A Category',title2:'Post a Product'})
 });
+
 
 
 const PORT = process.env.PORT || 5000;
--- routes/api/category.js
+++ routes/api/category.js
@@ -19,6 +19,44 @@ router.post('/',(req,res) =>{
    });
    res.status(200).json(newCategory);
 });
+router.post("/product",(req,res)=> {
+
+  if(!req.body.category || !req.body.description || !req.body.image
+    || !req.body.name || !req.body.price || !req.body.sku){
+      return
+    }
+
+    const productRef = db.collection("productList");
+    const newProduct = {
+      category: req.body.category,
+      description: req.body.description,
+      image: req.body.image,
+      name: req.body.name,
+      price: parseFloat(req.body.price),
+      sku: parseInt(req.body.sku)
+    }
+    
+    productRef.add({
+      category: req.body.category,
+      description: req.body.description,
+      image: req.body.image,
+      name: req.body.name,
+      price: parseFloat(req.body.price),
+      sku: parseInt(req.body.sku)
+
+    });
+    res.status(200).json(newProduct);
+
+    db.collection("productList")
+    .onSnapshot(function(querySnapshot) {
+        var products = [];
+        querySnapshot.forEach(function(doc) {
+            products.push(doc.data().name);
+        });
+        console.log("Current cities in CA: ", products.join(", "));
+    });
+
+});
 
 
   
